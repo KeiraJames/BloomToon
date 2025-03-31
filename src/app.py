c992ca6c009f9de22df9d0ecbc0bfd9ac4d317f8
Keira <keirajames2003@gmail.com>
2025-03-31 14:27:54 -0400

    Updated app.py to remove tests and added test_app.py in the tests folder

--- src/app.py
+++ src/app.py
@@ -4,9 +4,6 @@ def main():
     # Set up PlantNet API
     plantnet_api = PlantNetAPI(api_key="2b10X3YLMd8PNAuKOCVPt7MeUe")
     
-    # Example usage of PlantNetAPI
-    plant = plantnet_api.identify_plant("monstera.jpg")
-    print(plant)
 
 if __name__ == "__main__":
     main()
